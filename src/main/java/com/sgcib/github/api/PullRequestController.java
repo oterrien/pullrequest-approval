97924d8b6a2f24f46515f2ec9f15124d63a66ad2
OTerrien <oterrien@neuf.fr>
2016-03-11 21:08:55 +0100

    log headers

--- src/main/java/com/sgcib/github/api/PullRequestController.java
+++ src/main/java/com/sgcib/github/api/PullRequestController.java
@@ -2,6 +2,7 @@ package com.sgcib.github.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
 
 
 /**
@@ -26,11 +30,17 @@ public class PullRequestController {
     }
 
     @RequestMapping(method = RequestMethod.POST, name = "/")
-    public ResponseEntity<Boolean> onEvent(@RequestBody String requestWrapper) {
+    public ResponseEntity<Boolean> onEvent(HttpServletRequest request) {
 
-        eventFactory.getEventHandler("pull-request").
+        while(request.getHeaderNames().hasMoreElements()){
+            String name = request.getHeaderNames().nextElement();
+            System.out.println(name + ":" + request.getHeader(name));
+        }
+
+
+        /*eventFactory.getEventHandler("pull-request").
                 ifPresent(h-> h.handle(requestWrapper));
-
+*/
         return new ResponseEntity<Boolean>(true, HttpStatus.OK);
     }
 }
