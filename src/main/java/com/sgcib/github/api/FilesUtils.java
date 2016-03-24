fdd86568c15d9254f4e8972bfd3c86ab44b50c2b
Olivier TERRIEN <oterrien@neuf.fr>
2016-03-24 17:26:37 +0100

    Update FilesUtils.java

--- src/main/java/com/sgcib/github/api/FilesUtils.java
+++ src/main/java/com/sgcib/github/api/FilesUtils.java
@@ -9,8 +9,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
