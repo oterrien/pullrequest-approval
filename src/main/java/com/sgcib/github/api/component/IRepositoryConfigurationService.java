5135f695731426a80a7a351bb8296dddd89b9562
Olivier TERRIEN <oterrien@neuf.fr>
2016-03-24 17:27:30 +0100

    Update IRepositoryConfigurationService.java

--- src/main/java/com/sgcib/github/api/component/IRepositoryConfigurationService.java
+++ src/main/java/com/sgcib/github/api/component/IRepositoryConfigurationService.java
@@ -1,9 +1,6 @@
 package com.sgcib.github.api.component;
 
 import com.sgcib.github.api.json.Repository;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 public interface IRepositoryConfigurationService {
 
