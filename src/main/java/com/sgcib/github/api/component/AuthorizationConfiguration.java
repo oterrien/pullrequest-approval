2b03b0831902582bf8c39bfb0c0319b6b054d850
Olivier TERRIEN <oterrien@neuf.fr>
2016-03-24 17:27:59 +0100

    Update AuthorizationConfiguration.java

--- src/main/java/com/sgcib/github/api/component/AuthorizationConfiguration.java
+++ src/main/java/com/sgcib/github/api/component/AuthorizationConfiguration.java
@@ -3,7 +3,6 @@ package com.sgcib.github.api.component;
 import lombok.Getter;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
